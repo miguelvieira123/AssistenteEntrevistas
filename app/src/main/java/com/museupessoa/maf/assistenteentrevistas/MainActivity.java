7acfd4af1c71ee01b196157bb65a66ebeb30f40d
miguelvieira123 <a62819@alunos.uminho.pt>
2016-03-16 14:01:00 +0000

    new branch Dev.

--- app/src/main/java/com/museupessoa/maf/assistenteentrevistas/MainActivity.java
+++ app/src/main/java/com/museupessoa/maf/assistenteentrevistas/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // novo branch development para controlar as releases
     }
 }

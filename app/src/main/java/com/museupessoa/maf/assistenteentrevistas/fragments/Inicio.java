9e16c936ef00f59553f6241286759f93a7acf041
miguelvieira123 <a62819@alunos.uminho.pt>
2016-03-18 21:01:52 +0000

    Teste de FragmentPagerAdapter. Outra alternativa pode ser o FragmentStatePagerAdapter para que seja possivel guardar o estado de cada view.

---
+++ app/src/main/java/com/museupessoa/maf/assistenteentrevistas/fragments/Inicio.java
@@ -0,0 +1,19 @@
+package com.museupessoa.maf.assistenteentrevistas.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.museupessoa.maf.assistenteentrevistas.R;
+
+public class Inicio extends android.support.v4.app.Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.inicio_view, container, false);
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/museupessoa/maf/assistenteentrevistas/fragments/Projetos.java
@@ -0,0 +1,18 @@
+package com.museupessoa.maf.assistenteentrevistas.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.museupessoa.maf.assistenteentrevistas.R;
+
+public class Projetos extends android.support.v4.app.Fragment{
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.projetos_view, container, false);
+    }
+
+}

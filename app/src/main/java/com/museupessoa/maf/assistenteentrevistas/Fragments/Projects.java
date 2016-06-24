9aa879797faf29318e04a682b2996199ff5e8fc5
pg29026 <pg29026@alunos.uminho.pt>
2016-06-24 12:07:55 +0100

    Correções
    Fiz algumas correções, é preciso mudar o cabeçalho da form na parte da
    entrevista (está interview)

--- app/src/main/java/com/museupessoa/maf/assistenteentrevistas/Fragments/Projects.java
+++ app/src/main/java/com/museupessoa/maf/assistenteentrevistas/Fragments/Projects.java
@@ -131,13 +131,13 @@ public class Projects extends Fragment{
                 intent.putExtra("name",projectUnits.get(requestCode).name);
                 intent.putExtra("status",2);
                 startActivity(intent);
-                Toast.makeText(getActivity(),"EDIT",Toast.LENGTH_LONG).show();
+                Toast.makeText(getActivity(),"Editar",Toast.LENGTH_LONG).show();
                 break;
             case  Projects.DELETE_PROJECT:
                 android.support.v4.app.FragmentManager delProject = getFragmentManager();
                 DeleteProjectDialogFragment dialogProjectName = new DeleteProjectDialogFragment();
                 dialogProjectName.setTargetFragment(this, requestCode);
-                dialogProjectName.show(delProject, "DeleteProject");
+                dialogProjectName.show(delProject, "Eliminar");
                 break;
         }
     }
